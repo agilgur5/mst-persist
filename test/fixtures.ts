8417f22c1ce5710de36f664a5de33b5949d54e6b
Anton Gilgur <agilgur5@gmail.com>
2019-11-19 18:02:42 -0500

    (test): ensure transforms apply and do so in correct order
    - yay back to 100% code coverage now!
      - there's a transform branch missing bc there's no test case where
        there's something in storage and at least one toStorage-only
        transform
        - but covering this else branch doesn't really matter
    
    - create some transform fixtures
    - abstract out a setItem function
    
    - should consider splitting the tests and fixtures into separate files
      soon, esp now that we have a few different test suites here

--- test/fixtures.ts
+++ test/fixtures.ts
@@ -1,5 +1,7 @@
 import { types } from 'mobx-state-tree'
 
+import { ITransform, ITransformArgs } from '../src/index'
+
 export const UserStoreF = types.model('UserStore', {
   name: 'John Doe',
   age: 32
@@ -12,4 +14,20 @@ export const UserStoreF = types.model('UserStore', {
 export const persistedDataF = {
   name: 'Persisted Name',
   age: 35
+}
+
+function changeName (name: string) {
+  const changeNameTransform: ITransformArgs = function (snapshot) {
+    snapshot.name = name
+    return snapshot
+  }
+  return changeNameTransform
+}
+
+export function storeNameAsF (name: string): ITransform {
+  return {toStorage: changeName(name)}
+}
+
+export function retrieveNameAsF (name: string): ITransform {
+  return {fromStorage: changeName(name)}
 }
