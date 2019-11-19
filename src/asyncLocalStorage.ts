81b092716599707441251a676dff476c4c5f3f01
Anton Gilgur <agilgur5@gmail.com>
2019-11-18 22:52:10 -0500

    (fix): resolve AsyncLocalStorage Illegal Invocation errors
    - I (and apparently most mst-persist users) don't use localStorage, so
      this went undetected for a while until someone reported a bug
      - and I finally hit upon it myself when adding tests shortly after,
        which added a solid reproduction as well as a blocker on it
    
    - it made localStorage unusable as all of AsyncLocalStorage's calls
      would give Illegal Invocation errors
      - see https://stackoverflow.com/q/41126149/3431180 for more details
        on this error
        - using .call did not work, not on
          callWithPromise.call(window, ...) nor func.call(window, ...args)
        - similarly window.localStorage.func.bind(window) did not work
        - so instead used wrapper functions as an alternative that didn't
          feel too convoluted
          - using window.localStorage[funcKey] in callWithPromise did not
            quite feel right, wrapper functions felt better

--- src/asyncLocalStorage.ts
+++ src/asyncLocalStorage.ts
@@ -6,17 +6,18 @@ interface IAsyncLocalStorage {
 }
 
 export const AsyncLocalStorage: IAsyncLocalStorage = {
+  // must use wrapper functions when passing localStorage functions (https://github.com/agilgur5/mst-persist/issues/18)
   clear () {
-    return callWithPromise(window.localStorage.clear)
+    return callWithPromise(() => window.localStorage.clear())
   },
   getItem (key) {
-    return callWithPromise(window.localStorage.getItem, key)
+    return callWithPromise(() => window.localStorage.getItem(key))
   },
   removeItem (key) {
-    return callWithPromise(window.localStorage.removeItem, key)
+    return callWithPromise(() => window.localStorage.removeItem(key))
   },
   setItem (key, value) {
-    return callWithPromise(window.localStorage.setItem, key, value)
+    return callWithPromise(() => window.localStorage.setItem(key, value))
   }
 }
 
